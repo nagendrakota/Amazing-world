f51c5ea26794d2ad716fc1017ef7798a4b2a6414
Nagendra <nagforyou143@gmail.com>
2021-02-20 20:59:59 +0530

    updated

---
+++ Nage/Nage/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Nage
+//
+//  Created by Nagendra Babu on 20/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    var age = 27
+    var name = "hello"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
